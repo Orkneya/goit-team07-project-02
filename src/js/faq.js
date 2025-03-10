fb467c157e2fc4b55a88f666ab023aa5f0108cb1
yegor <yegorveretelnik@gmail.com>
2025-03-10 13:22:08 +0100

    commit message

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,7 @@
 // @ts-ignore
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css"; 
 
-new Accordion(".accordion-container", {
-    duration: 400,  
-    showMultiple: true,
-    elementClass: "accordion-item",
-    triggerClass: "accordion-button",
-    panelClass: "accordion-content",
-    
 
-});
+
+
+
 
