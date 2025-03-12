737c4979e09f9932dba4623f68d33830b3d2c6cc
Veronika0136 <Artemova331@gmail.com>
2025-03-12 02:40:43 +0200

    update skills

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -36,25 +36,6 @@ new Swiper(".about-skills-swiper", {
         slidesPerView: 6,
       },
     },
+});
 
-    on: {
-      slideChangeTransitionEnd: function () {
-        document.querySelectorAll(".about-skills-txt").forEach((e) => {
-          e.classList.remove("ellips-red");
-        });
-  
-        const visibleSlides = document.querySelectorAll(".swiper-slide");
-        const firstVisibleSlide = Array.from(visibleSlides).find((slide) => {
-          return slide.getBoundingClientRect().left >= 0;
-        });
-  
-        if (firstVisibleSlide) {
-          const firstTextElement =
-            firstVisibleSlide.querySelector(".about-skills-txt");
-          if (firstTextElement) {
-            firstTextElement.classList.add("ellips-red");
-          }
-        }
-      },
-    },
-  });
+ 
