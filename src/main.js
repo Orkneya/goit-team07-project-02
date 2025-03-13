8f7565df407d55d3301fc2b6ab9df4ef1edff56b
Veronika0136 <Artemova331@gmail.com>
2025-03-13 15:13:55 +0200

    update footer

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/css';
 import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import './js/header';
 import './js/about-me';
@@ -12,4 +14,4 @@ import './js/faq';
 import './js/reviews';
 import './js/mobile';
 import './js/projects';
-import './js/modal-window';
+import './js/footer';
