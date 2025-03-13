7e0d560c0b0db88cf51cada69aea1efc72e1ad7f
Veronika0136 <Artemova331@gmail.com>
2025-03-13 14:55:49 +0200

    update

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("modal");
-    const openModalBtn = document.getElementById("openModal");
-    const closeModalBtn = document.getElementById("closeModal");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const modal = document.getElementById("modal");
+//     const openModalBtn = document.getElementById("openModal");
+//     const closeModalBtn = document.getElementById("closeModal");
     
-    function openModal() {
-        modal.classList.add("active");
-        document.body.classList.add("modal-open");
-    }
+//     function openModal() {
+//         modal.classList.add("active");
+//         document.body.classList.add("modal-open");
+//     }
     
-    function closeModal() {
-        document.body.classList.remove("modal-open");
-    }
+//     function closeModal() {
+//         document.body.classList.remove("modal-open");
+//     }
     
-    openModalBtn.addEventListener("click", openModal);
-    closeModalBtn.addEventListener("click", closeModal);
+//     openModalBtn.addEventListener("click", openModal);
+//     closeModalBtn.addEventListener("click", closeModal);
     
-    modal.addEventListener("click", (event) => {
-       if (event.target === closeModalBtn || event.target === modal) {
-        closeModal();
-       }
-    });
+//     modal.addEventListener("click", (event) => {
+//        if (event.target === closeModalBtn || event.target === modal) {
+//         closeModal();
+//        }
+//     });
     
-    document.addEventListener("keydown", (event) =>  {
-        if (event.key === "Escape") {
-            closeModal();
-        }
-     });
-    });
+//     document.addEventListener("keydown", (event) =>  {
+//         if (event.key === "Escape") {
+//             closeModal();
+//         }
+//      });
+//     });
     
