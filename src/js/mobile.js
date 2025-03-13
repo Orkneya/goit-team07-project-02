5c096593ac786224c4ca720d51e95b61fcdad6ea
Roman Baskov <rvbaskov@gmail.com>
2025-03-13 14:36:04 +0300

    update

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,17 +1,17 @@
 (() => {
   const refs = {
-    // openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
+    openModalBtn1: document.querySelector('[data-menu-open]'),
+    closeModalBtn1: document.querySelector('[data-menu-close]'),
     modal: document.querySelector('[data-menu]'),
   };
-console.log(refs.closeModalBtn);
+  console.log(refs.closeModalBtn);
 
-  // refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.closeModalBtn1.addEventListener('click', toggleModal);
 
   function toggleModal() {
     console.log(refs.modal);
-    
+
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
   }
