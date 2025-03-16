b83ff55f89cb2526bb5c1504d0ff3eedfd418e32
Tetyana Popenko <orkneya17@gmail.com>
2025-03-16 22:59:28 +0200

    all-error

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,25 +1,6 @@
-// (() => {
-//   const refs = {
-//     openModalBtn1: document.querySelector('[data-menu-open]'),
-//     closeModalBtn1: document.querySelector('[data-menu-close]'),
-//     modal: document.querySelector('[data-menu]'),
-//   };
-//   console.log(refs.closeModalBtn);
-
-//   refs.openModalBtn1.addEventListener('click', toggleModal);
-//   refs.closeModalBtn1.addEventListener('click', toggleModal);
-
-//   function toggleModal() {
-//     console.log(refs.modal);
-
-//     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//     refs.modal.classList.toggle('is-open');
-//   }
-// })();
-
-const menu = document.querySelector('.menu__boby');
+const menu = document.querySelector('[data-menu] ');
 const menuBtn = document.querySelector('.menu__icon');
-const btnClose = document.querySelector('.menu-close');
+const btnClose = document.querySelector('[data-menu-close]');
 
 const boby = document.body;
 
@@ -31,18 +12,16 @@ if (menu && menuBtn) {
   });
 
   menu.querySelectorAll('.menu-link').forEach(link => {
-    link.addEventListener('click', () => {
-      menu.classList.remove('is-open');
-      menuBtn.classList.remove('active');
-      boby.classList.remove('lock');
-    });
+    link.addEventListener('click', toggleModal);
   });
 }
 
 if (btnClose) {
-  btnClose.addEventListener('click', () => {
-    menu.classList.remove('is-open');
-    menuBtn.classList.remove('active');
-    boby.classList.remove('lock');
-  });
+  btnClose.addEventListener('click',toggleModal);
+}
+
+function toggleModal() {
+  menu.classList.remove('is-open');
+  menuBtn.classList.remove('active');
+  boby.classList.remove('lock');
 }
